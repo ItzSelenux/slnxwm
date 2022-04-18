a58377ddd0b43b94ffecabfd54825672055c0764
ItzSelenux.outlook.com <ItsZariep@outlook.com>
2022-04-18 19:41:01 +0000

    .

--- themes/gruvbox.h
+++ themes/gruvbox.h
@@ -4,8 +4,9 @@ static const char c_bg[]            = "#282828";
 static const char c_fg[]            = "#928374";
 
 //Colors
-static const char c_fg_green[]      = "#b8bb26"; 
-static const char c_fg_yellow[]     = "#fabd2f"; 
-static const char c_fg_blue[]       = "#83a598"; 
-static const char c_fg_purple[]     = "#d3869f"; 
-static const char c_fg_orange[]     = "#fe8019"; 
+static const char c_fg_aqua[]       = "#689d6a"; 
+static const char c_fg_green[]      = "#98971a"; 
+static const char c_fg_yellow[]     = "#d79921"; 
+static const char c_fg_blue[]       = "#458588"; 
+static const char c_fg_purple[]     = "#b16286"; 
+static const char c_fg_orange[]     = "#d65d0e"; 
